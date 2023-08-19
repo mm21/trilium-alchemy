202f7cc613b42fc7559a02dd531c32b9841b0321
Jhon Honce <jhonce@redhat.com>
2023-08-19 01:18:23 -0700

    Convert tests to use pytest.mark fixture
    Signed-off-by: Jhon Honce <jhonce@redhat.com>

--- test/note/test_note_attributes.py
+++ test/note/test_note_attributes.py
@@ -7,31 +7,24 @@ Test basic CRUD capability of note's attributes.
 """
 
 
+@pytest.mark.attribute("Label1", "Value")
 def test_set(session: Session, note: Note):
     # set / in attribute
-    note["Label1"] = "Value"
-    note.flush()
     assert "Label1" in note
     assert note.attributes["Label1"][0].value == "Value"
 
 
+@pytest.mark.attribute("Label2", "Value")
 def test_update(session: Session, note: Note):
     # update / get attribute
-    note["Label2"] = "Value"
-    note.flush()
-    assert note.attributes["Label2"][0].value == "Value"
-
     note["Label2"] = "New Value"
     note.flush()
     assert note["Label2"] == "New Value"
 
 
+@pytest.mark.attribute("Label3", "Value")
 def test_del(session: Session, note: Note):
     # del / not in attribute
-    note["Label3"] = "Value"
-    note.flush()
-    assert note.attributes["Label3"][0].value == "Value"
-
     del note["Label3"]
     assert len(note) == 0
     assert "Label3" not in note
@@ -46,15 +39,13 @@ def test_get(session: Session, note: Note):
     assert note.get("Label4", "Value") == "Value"
 
 
+@pytest.mark.attribute("Label5", "Value5")
+@pytest.mark.attribute("Label6", "Value6")
+@pytest.mark.attribute("Label7", "Value7")
+@pytest.mark.attribute("Label8", "Value8")
+@pytest.mark.attribute("Label9", "Value9")
 def test_iter(session: Session, note: Note):
-    # iterate over attributes
-    note["Label5"] = "Value5"
-    note["Label6"] = "Value6"
-    note["Label7"] = "Value7"
-    note["Label8"] = "Value8"
-    note["Label9"] = "Value9"
-    note.flush()
-
+    # len / iterate over attributes
     assert len(note) == 5
 
     keys = [
