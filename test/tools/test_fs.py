e4c4eeb582f6e52b7e92889bab181931b3e9a298
mm21 <mm21.dev@gmail.com>
2025-05-31 16:52:51 -0700

    Add load_notes() and verify

--- test/tools/test_fs.py
+++ test/tools/test_fs.py
@@ -5,10 +5,10 @@ Test filesystem dump/load tool functionality.
 from pathlib import Path
 
 from trilium_alchemy import *
-from trilium_alchemy.tools.fs import dump_notes
+from trilium_alchemy.tools.fs import dump_notes, load_notes
 
 from ..conftest import compare_folders
-from ..fs_utils import create_note_1
+from ..fs_utils import NOTE_1_ID, check_note_1, create_note_1
 
 FS_NOTES_PATH = Path(__file__).parent / "fs-dump"
 
@@ -19,7 +19,7 @@ def test_dump(session: Session, note: Note, tmp_path: Path):
     """
 
     note_1 = create_note_1(session, note)
-    dump_notes([note_1], tmp_path, recursive=True)
+    dump_notes(tmp_path, [note_1], recursive=True)
 
     # compare tmp_path with expected path
     compare_folders(tmp_path, FS_NOTES_PATH)
@@ -34,7 +34,7 @@ def test_dump(session: Session, note: Note, tmp_path: Path):
     unexpected_file.write_text("")
 
     # dump with pruning
-    dump_notes([note_1], tmp_path, recursive=True, prune=True)
+    dump_notes(tmp_path, [note_1], recursive=True, prune=True)
 
     assert unexpected_folder.exists()
     assert not unexpected_pruned_folder.exists()
@@ -44,3 +44,18 @@ def test_dump(session: Session, note: Note, tmp_path: Path):
     unexpected_folder.rmdir()
     unexpected_file.unlink()
     compare_folders(tmp_path, FS_NOTES_PATH)
+
+
+def test_load(session: Session, note: Note):
+    """
+    Load note hierarchy from folder.
+    """
+
+    notes = load_notes(FS_NOTES_PATH, session, parent_note=note)
+
+    # find and check note 1
+    note_1 = next(n for n in notes if n.note_id == NOTE_1_ID)
+    check_note_1(note_1, State.CREATE)
+
+    assert len(note.children)
+    assert note.children[0] is note_1
--- trilium_alchemy/tools/fs.py
+++ trilium_alchemy/tools/fs.py
@@ -11,6 +11,7 @@ from pathlib import Path
 
 from ..core.note._fs import METADATA_FILENAME
 from ..core.note.note import Note
+from ..core.session import Session
 
 __all__ = [
     "dump_notes",
@@ -44,8 +45,8 @@ Number of characters in note folder.
 
 
 def dump_notes(
-    notes: list[Note],
     dest_dir: Path,
+    notes: list[Note],
     *,
     recursive: bool = False,
     prune: bool = False,
@@ -76,6 +77,38 @@ def dump_notes(
         _prune_dirs(dest_dir, dumped_note_dirs)
 
 
+def load_notes(
+    src_dir: Path,
+    session: Session,
+    *,
+    parent_note: Note | None = None,
+) -> list[Note]:
+    """
+    Load notes from source folder and optionally place top-level notes as
+    children of parent note.
+    """
+
+    notes: list[Note] = []
+    note_dirs = _find_note_dirs(src_dir)
+
+    # load notes
+    for note_dir in note_dirs:
+        note = Note.load_fs(note_dir, session)
+        notes.append(note)
+
+    # if parent given, find relative root notes and add as children
+    if parent_note:
+        children: set[Note] = set(parent_note.children)
+        root_notes = _find_root_notes(notes)
+
+        # add children sorted by title, if not already present
+        for note in sorted(root_notes, key=lambda n: n.title):
+            if note not in children:
+                parent_note += root_notes
+
+    return notes
+
+
 def _aggregate_notes(notes: list[Note]) -> list[Note]:
     """
     Aggregate notes and children recursively.
@@ -92,6 +125,75 @@ def _aggregate_notes(notes: list[Note]) -> list[Note]:
         walk(note)
 
     return sorted(aggregated_notes, key=lambda n: n.note_id)
+
+
+def _find_note_dirs(
+    root_dir: Path, empty_dirs: list[Path] | None = None
+) -> list[Path]:
+    """
+    Walk prefix tree folder and return valid note folders, logging warnings
+    for any unexpected files/folders. Optionally populates list of empty
+    folders.
+    """
+
+    note_dirs: list[Path] = []
+
+    def check_note_dir(dir_path: Path):
+        """
+        Check if this is a valid note folder and add to note dirs.
+        """
+
+        # ensure name is of expected length
+        if len(dir_path.name) != SUFFIX_SIZE:
+            logging.warning(f"Unexpected folder: '{dir_path}'")
+            return
+
+        if METADATA_FILENAME in (p.name for p in dir_path.iterdir()):
+            note_dirs.append(dir_path)
+        else:
+            logging.warning(
+                f"Note folder '{dir_path}' does not contain metadata file"
+            )
+
+    def check_prefix_dir(dir_path: Path) -> bool:
+        """
+        Check if this folder is a valid prefix folder.
+        """
+
+        # ensure name is of expected length
+        if len(dir_path.name) != PREFIX_SIZE:
+            logging.warning(f"Unexpected folder: '{dir_path}'")
+            return False
+
+        # check for empty folder
+        if empty_dirs is not None and next(dir_path.iterdir(), None) is None:
+            empty_dirs.append(dir_path)
+
+        return True
+
+    def recurse(dir_path: Path, depth: int = 0):
+        for path in dir_path.iterdir():
+            # ensure not a file
+            if path.is_file():
+                logging.warning(f"Unexpected file: '{path}'")
+                continue
+
+            # ensure dir name is a hex string
+            try:
+                int(path.name, base=16)
+            except ValueError:
+                logging.warning(f"Unexpected folder: '{path}'")
+                continue
+
+            if depth == TREE_DEPTH:
+                check_note_dir(path)
+            else:
+                if check_prefix_dir(path):
+                    recurse(path, depth=depth + 1)
+
+    recurse(root_dir)
+
+    return note_dirs
 
 
 def _map_note_dir(note: Note) -> Path:
@@ -123,63 +225,8 @@ def _prune_dirs(root_dir: Path, dumped_note_dirs: list[Path]):
     Remove existing paths not belonging to dumped notes.
     """
 
-    note_dirs: list[Path] = []
     empty_dirs: list[Path] = []
-
-    def check_note_dir(dir_path: Path):
-        """
-        Check if this is a valid note folder and add to note dirs.
-        """
-
-        # ensure name is of expected length
-        if len(dir_path.name) != SUFFIX_SIZE:
-            logging.warning(f"Unexpected folder: '{dir_path}'")
-            return
-
-        if METADATA_FILENAME in (p.name for p in dir_path.iterdir()):
-            note_dirs.append(dir_path)
-        else:
-            logging.warning(
-                f"Note folder '{dir_path}' does not contain metadata file"
-            )
-
-    def check_prefix_dir(dir_path: Path) -> bool:
-        """
-        Check if this folder is a valid prefix folder.
-        """
-
-        # ensure name is of expected length
-        if len(dir_path.name) != PREFIX_SIZE:
-            logging.warning(f"Unexpected folder: '{dir_path}'")
-            return False
-
-        # check for empty folder
-        if next(dir_path.iterdir(), None) is None:
-            empty_dirs.append(dir_path)
-
-        return True
-
-    def recurse(dir_path: Path, depth: int = 0):
-        for path in dir_path.iterdir():
-            # ensure not a file
-            if path.is_file():
-                logging.warning(f"Unexpected file: '{path}'")
-                continue
-
-            # ensure dir name is a hex string
-            try:
-                int(path.name, base=16)
-            except ValueError:
-                logging.warning(f"Unexpected folder: '{path}'")
-                continue
-
-            if depth == TREE_DEPTH:
-                check_note_dir(path)
-            else:
-                if check_prefix_dir(path):
-                    recurse(path, depth=depth + 1)
-
-    recurse(root_dir)
+    note_dirs = _find_note_dirs(root_dir, empty_dirs)
 
     # determine which note paths to prune
     stale_note_dirs = sorted(set(note_dirs) - set(dumped_note_dirs))
@@ -214,3 +261,22 @@ def _normalize_note_id(note_id: str) -> str:
     cryptographically optimal than discarding bits from SHA-256 hash.
     """
     return hashlib.shake_128(note_id.encode(encoding="utf-8")).hexdigest(16)
+
+
+def _find_root_notes(notes: list[Note]) -> list[Note]:
+    """
+    Traverse notes loaded from filesystem and find ones which are "relative"
+    roots, i.e. don't have any parents which are present on the filesystem.
+    """
+
+    root_notes: list[Note] = []
+    notes_set = set(notes)
+
+    for note in notes:
+        if not any(p in notes_set for p in note.parents):
+            root_notes.append(note)
+
+    # there must be at least one relative root note or else there is a cycle
+    assert len(root_notes)
+
+    return root_notes
